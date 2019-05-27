05c318df9f7a26000e716ba176b50027146fd22d
Arman Golmakan <arman.golmakan@caseware.com>
2019-05-27 17:48:54 -0400

    refined server.js, used angularjs tutorial as example

--- server.js
+++ server.js
@@ -4,33 +4,39 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-app.get('/scrape', function (req, res) {
+app.get('/scraper', function (req, res) {
 
-    url = 'https://www.caseware.com';
-
+    url = 'https://docs.angularjs.org/tutorial';
 
     request(url, function (error, response, html) {
         if (!error) {
-            console.log('initial request completed with no error');
             var $ = cheerio.load(html);
 
-            var title, release, rating;
-            var json = { txt: "" };
+            var json = {txt : ''};
 
-            let mytxt = $('title');
-            console.log(mytxt);
+            $('head').filter(function () {
+                // var data = $(this);
+                json.txt = this.namespace;
+            })
 
         }
 
- 
-        
+        // Parameter 1 :  output.json - name of file which will get created
+        // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+        // Parameter 3 :  callback function - a callback function to let us know the status of our function
+
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
+
+            console.log('File successfully written! - Check your project directory for the output.json file');
+
+        })
 
         // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
         res.send('Check your console!')
 
     });
 })
-
 app.listen('8081');
-console.log('Magic happens on port 8081');
+console.log('Listening on port 8081. do node --inspect server.js to do chrome debugging');
 exports = module.exports = app;
+
